05ba18cfd8a2ec59a775049c7386795606c8b272
arvindalawa07 <arvindalawa1998@gmail.com>
2023-12-07 15:13:05 +0530

    changes on json

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -16,7 +16,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "https://navneet-setoo.myshopify.com/",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage({prisma,"tableName": ''}),
+  sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
   restResources,
   webhooks: {
